a59caf8f0dc173defff6bac71e37f7f428e07349
Aaryan <aaryan@browserstack.com>
2024-02-21 21:12:36 +0530

    added python w3c script

--- README.md
+++ README.md
@@ -15,9 +15,15 @@ Selenium -> test-file-upload
 
 1. [Ruby](https://automate.browserstack.com/builds/7c3e0bf186848b3400e9cb4f263eb9ef67fcaf9a/sessions/82c16418c15a7d34c3cf8fc7b44ecfc13eee3aae?auth_token=9dd92c85fedc0d4d38c5f4e9c5de54b2706d561883575cd3448a1c04397dc88f)
 
-2.
+2. [Python](https://automate.browserstack.com/builds/7c3e0bf186848b3400e9cb4f263eb9ef67fcaf9a/sessions/fa07830ceb5527b6abd980c9b80c298b0266ddf9?auth_token=f373b3d56017745d46d377b933cd41615f11f6a4d9fe61cf529725c27016ce94)
 
 ### Selenium Legacy JSON test scripts to test on iOS devices
 
 1. [Ruby](https://automate.browserstack.com/builds/2f18512e248860fa123b747b27c46b90ad43079d/sessions/172a214ca357e1c0923c805aeb1f8706fe9c6ce0?auth_token=a7eea0345d33c5031e1a40a451ce2690649ce037af1938f5476efce2b4ef5573)
-2.
+
+2. [Python](https://automate.browserstack.com/builds/c8d3ee09bf752e26d03c156d69520a328f41c476/sessions/19e4d9961a29b48a709d0a6556fb8334bfadd322?auth_token=b5e705ac702d4d0e7c49466d9e1d7466d735dbb1207735f330969987975b42df)
+
+Note:
+1. Python: Only Appium v1 supports Legacy JSON, therefore for those scripts to work use the following versions:
+  - Appium-Python-Client==1.3.0
+  - urllib3==1.26.16
---
+++ W3C-ios-scripts/python_script.py
@@ -0,0 +1,44 @@
+from appium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common import action_chains, keys
+from time import sleep
+from selenium.webdriver.common.by import By
+from appium.options.ios import XCUITestOptions
+
+desired_cap = {
+	'bstack:options' : {
+		"osVersion" : "14",
+		"deviceName" : "iPhone 12",
+		"realMobile" : "true",
+		"projectName" : "Sample Test",
+		"buildName" : "Sample_test",
+		"debug" : "true",
+		"userName": "username",
+    "accessKey": "access-key"
+	},
+	"appium:nativeWebTap": True,
+}
+options = XCUITestOptions()
+options.set_capability('appium:nativeWebTap', True)
+options.set_capability('appium:automationName', 'XCUITest')
+options.set_capability('bstack:options', desired_cap['bstack:options'])
+
+driver = webdriver.Remote(
+    command_executor='https://hub.browserstack.com/wd/hub',
+    options=options)
+
+driver.get("https://the-internet.herokuapp.com/upload")
+sleep(10)
+driver.find_element(By.ID, 'file-upload').click()
+driver.switch_to.context('NATIVE_APP')
+driver.find_element(By.NAME, 'Photo Library').click()
+sleep(5)
+elements = driver.find_elements(By.CLASS_NAME, "XCUIElementTypeImage")
+elements[0].click() # 1 represents second element from the list of 9 preloaded images and videos
+sleep(10)
+driver.find_element(By.NAME, 'Choose').click()
+sleep(10)
+driver.switch_to.context(driver.contexts[1])
+driver.find_element(By.ID, "file-submit").click()
+driver.quit()
