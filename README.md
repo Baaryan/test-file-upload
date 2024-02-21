6a5b829e423d17e343c61abd1c216603076ea53e
Aaryan <aaryan@browserstack.com>
2024-02-21 18:08:08 +0530

    Added details & ruby script

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# test-file-upload
+# Test-file-upload
 Selenium -> test-file-upload
+
+## Documentation
+
+[Link](https://www.browserstack.com/docs/automate/selenium/test-file-upload)
+
+## Jira
+
+[Link](https://browserstack.atlassian.net/browse/APS-9830)
+
+## Public Sessions to see results:
+
+### Selenium 4 W3C test scripts to test on iOS devices
+
+1. [Ruby](https://automate.browserstack.com/builds/7c3e0bf186848b3400e9cb4f263eb9ef67fcaf9a/sessions/82c16418c15a7d34c3cf8fc7b44ecfc13eee3aae?auth_token=9dd92c85fedc0d4d38c5f4e9c5de54b2706d561883575cd3448a1c04397dc88f)
+
+2.
+
+### Selenium Legacy JSON test scripts to test on iOS devices
+
+1. 
+2.
---
+++ W3C-ios-scripts/ruby_script.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'appium_lib'
+
+# Input capabilities
+capabilities = {
+	'bstack:options' => {
+		"osVersion" => "14",
+		"deviceName" => "iPhone 12",
+		"realMobile" => "true",
+		"projectName" => "Sample Test",
+		"buildName" => "Sample_test",
+		"debug" => "true",
+	},
+  "appium:nativeWebTap" => true
+}
+
+appium_driver = Appium::Driver.new({'caps' => capabilities,'appium_lib' => {:server_url => "https://<<username>>:<<access-key>>@hub-cloud.browserstack.com/wd/hub"}}, true)
+driver = appium_driver.start_driver
+driver.navigate.to "https://the-internet.herokuapp.com/upload"
+sleep(5)
+driver.find_element(xpath: "//*[@id='file-upload']").click
+driver.set_context('NATIVE_APP')
+sleep(10)
+driver.find_element(name: "Photo Library").click
+sleep(5)
+elements=driver.find_elements(:class_name,"XCUIElementTypeImage")
+elements[0].click
+driver.find_element(name: "Choose").click
+contexts = driver.available_contexts
+driver.set_context contexts[1]
+driver.find_element(:id,"file-submit").click()
+driver.quit()
