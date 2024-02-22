c3d6ba84cbd08045ecdc9cc49c1667ea4b6de841
Devashree Parvakar <devashreeparvakar@Devashrees-MacBook-Pro.local>
2024-02-22 22:57:37 +0530

    Adding Java Test Scripts

---
+++ Legacy-ios-scripts/java_script.java
@@ -0,0 +1,37 @@
+import java.net.URL;
+import java.util.List;
+import java.util.Set;
+import java.net.MalformedURLException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+public class Upload extends Thread{
+   public static String userName = "";
+   public static String accessKey = "";
+   public static void main(String args[]) throws MalformedURLException, InterruptedException {
+     DesiredCapabilities caps = new DesiredCapabilities();
+     caps.setCapability("device", "iPhone 12 Pro Max");
+     caps.setCapability("os_version", "14");
+     caps.setCapability("real_mobile", "true");
+     caps.setCapability("project", "My First Project");
+     caps.setCapability("build", "My First Build");
+     caps.setCapability("name", "Bstack-[Java] Sample Test");
+     caps.setCapability("nativeWebTap", "true");
+     IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), caps);
+     driver.get("https://the-internet.herokuapp.com/upload");
+     Thread.sleep(5000);
+     driver.findElement(By.id("file-upload")).click();
+     driver.context("NATIVE_APP");
+     driver.findElement(By.name("Photo Library")).click();
+     Thread.sleep(5000);
+     List list = driver.findElements(By.className("XCUIElementTypeImage"));
+    ((IOSElement) list.get(0)).click();
+     Thread.sleep(5000);
+     driver.findElement(By.name("Choose")).click();
+     Set<String> contextName = driver.getContextHandles();
+     driver.context(contextName.toArray()[1].toString());
+     driver.findElement(By.id("file-submit")).click();
+     driver.quit();
+  }
+}
---
+++ W3C-ios-scripts/java_script.java
@@ -0,0 +1,42 @@
+
+import java.net.URL;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Set;
+import java.net.MalformedURLException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+public class Upload extends Thread{
+   public static String userName = "";
+   public static String accessKey = "";
+   public static void main(String args[]) throws MalformedURLException, InterruptedException {
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
+    browserstackOptions.put("osVersion", "14");
+    browserstackOptions.put("deviceName", "iPhone 12");
+    browserstackOptions.put("realMobile", "true");
+    browserstackOptions.put("projectName", "Sample Test");
+    browserstackOptions.put("buildName", "Sample Build");
+    browserstackOptions.put("debug", "true");
+    capabilities.setCapability("bstack:options", browserstackOptions);
+    capabilities.setCapability("appium:nativeWebTap",true);
+     IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), capabilities);
+     driver.get("https://the-internet.herokuapp.com/upload");
+     Thread.sleep(5000);
+     driver.findElement(By.id("file-upload")).click();
+     driver.context("NATIVE_APP");
+     driver.findElement(By.name("Photo Library")).click();
+     
+     Thread.sleep(5000);
+     List list = driver.findElements(By.className("XCUIElementTypeImage"));
+    ((IOSElement) list.get(0)).click();
+     Thread.sleep(5000);
+     driver.findElement(By.name("Choose")).click();
+     Set<String> contextName = driver.getContextHandles();
+     driver.context(contextName.toArray()[1].toString());
+     driver.findElement(By.id("file-submit")).click();
+     driver.quit();
+  }
+}
