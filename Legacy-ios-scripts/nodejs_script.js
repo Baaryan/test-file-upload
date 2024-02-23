36ca7d74a41f2c52a390a0d6c571144389c9e867
Devashree Pravakar <157594414+dvspvk@users.noreply.github.com>
2024-02-23 18:14:57 +0530

    Adding nodejs_script

---
+++ Legacy-ios-scripts/nodejs_script.js
@@ -0,0 +1,35 @@
+var wd = require('wd');
+// Input capabilities
+const capabilities = {
+ 'device' : 'iPhone 12',
+ 'realMobile' : 'true',
+ 'os_version' : '14.0',
+ 'browserName' : 'iPhone',
+ 'name': 'BStack-[NodeJS] Sample Test',
+ 'build': 'BStack Build Number 1',
+  "nativeWebTap":true
+}
+async function runTestWithCaps () {
+  let driver = wd.promiseRemote("https://<username>:<access-key>@hub-cloud.browserstack.com/wd/hub");
+  await driver.init(capabilities);
+  await driver.get("https://the-internet.herokuapp.com/upload")
+  await new Promise(r => setTimeout(r, 2000));
+  element = await driver.waitForElementById('file-upload')
+  await element.click()
+  await driver.context('NATIVE_APP')
+  element = await driver.waitForElementByName('Photo Library')
+  await element.click()
+  await new Promise(r => setTimeout(r, 2000));
+  element = await driver.elementsByClassName('XCUIElementTypeImage')
+  await element[0].click()
+  await new Promise(r => setTimeout(r, 5000));
+  element = await driver.waitForElementByName('Choose')
+  await element.click()
+  await new Promise(r => setTimeout(r, 10000));
+  contexts = await driver.contexts();
+  await driver.context(contexts[1])
+  element = await driver.waitForElementById("file-submit")
+  await element.click()
+  await driver.quit();
+}
+runTestWithCaps();
